f9e8aacf17bea591846768841420ef75be8bdb8d
hovanchuongse <hovanchuongse@gmail.com>
2019-05-10 09:01:48 +0700

    Update CV

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
 
-RewriteEngine on
-RewriteCond %{REQUEST_FILENAME} !-d
-RewriteCond %{REQUEST_FILENAME}\.html -f
-RewriteRule ^(.*)$ $1.html
-
-RewriteCond %{REQUEST_FILENAME}\.php -f
-RewriteRule ^(.*)$ $1.php
-
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php header( 'Location: /home.html' ) ;  ?>
